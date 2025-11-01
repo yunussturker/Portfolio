d244c95b3ebbe36b126d97f83bddc4b088f4c67f
Yunus Emre <111645885+yunussturker@users.noreply.github.com>
2025-11-01 21:55:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,59 +9,16 @@ var typed = new Typed(".text", {
   loop: true
 });
 
-// Function to Scroll to Section
+// Scroll to Section
 function scrollToSection(sectionID) {
   const section = document.querySelector(sectionID);
   if (section) {
-    const headerOffset = 100; // İstediğiniz kaydırma miktarını ayarlayabilirsiniz
+    const headerOffset = 100;
     const elementPosition = section.getBoundingClientRect().top;
     const offsetPosition = elementPosition - headerOffset;
-
-    window.scrollBy({
-      top: offsetPosition,
-      behavior: 'smooth',
-    });
+    window.scrollBy({ top: offsetPosition, behavior: 'smooth' });
   }
 }
 
-// Event Listeners for Scroll to Section
-document.querySelector("a[href='#home']").addEventListener("click", function () {
-  scrollToSection(".home");
-});
-
-document.querySelector("a[href='#about']").addEventListener("click", function () {
-  scrollToSection(".about");
-});
-
-document.querySelector("a[href='#skills']").addEventListener("click", function () {
-  scrollToSection(".skills");
-});
-
-let element = document.querySelector("#veri");
-let addHero = document.querySelector("#ekle");
-let new_ul = document.querySelector("ul#Mid-Lane");
-let clickCount = 0;
-
-function checkClicks() {
-  if (clickCount === 20) {
-    localStorage.clear();
-    clickCount = 0;
-    alert('Local storage cleared!');
-  }
-}
-
-addHero.addEventListener("click", function () {
-  let new_li = document.createElement("li");
-  new_li.innerHTML = element.value;
-  new_ul.appendChild(new_li);
-  element.value = "";
-  localStorage.setItem("heroes", new_ul.innerHTML);
-  clickCount++;
-  checkClicks();
-});
-
-window.addEventListener("load", function () {
-  if (localStorage.getItem("heroes")) {
-    new_ul.innerHTML = localStorage.getItem("heroes");
-  }
-});
+document.querySelector("a[href='#home']").addEventListener("click", () => scrollToSection(".home"));
+document.querySelector("a[href='#about']").addEventListener("click", () => scrollToSection(".about"));
